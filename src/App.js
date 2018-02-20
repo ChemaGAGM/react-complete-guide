78e8fbdbdb034a2f322872ba80dcc0a1ee946d0d
jmgarcia <jmgarcia@interiorvista.net>
2018-02-20 12:35:20 +0100

    Passing properties down the component tree; component state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+      persons: [
+          { name: 'Chema', age: 34 },
+          { name: 'Guiomar', age: 34 },
+          { name: 'Pablo', age: 55 }
+      ]
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-          <Person/>
+          <h1>Hi, I'm a React App</h1>
+          <button>Switch name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >My hobbies: racing</Person>
       </div>
     );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'I am a React App'))
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,13 @@
 
 import React from 'react';
 
-const person = () => <p>I'm a Person!</p>
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm { props.name } and I am { props.age }!</p>
+            <p>{ props.children }</p>
+        </div>
+    );
+}
 
 export default person;
